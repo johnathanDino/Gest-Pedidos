634f0bc01eb46256d5cb110b8d98a0618df09ebc
Jonathan Yoel Dino <jonathan_dino@hotmail.com>
2025-06-06 08:51:12 -0300

    Clase Client

--- src/main/java/clients/Client.java
+++ src/main/java/clients/Client.java
@@ -2,10 +2,40 @@ package clients;
 
 public class Client {
 
-    private int idCliente;
-    private String nombre;
-    private String apellido;
+    private int idClient;
+    private String name;
+    private String lastname;
     private String mail;
-    private String contraseña;
+    private String password;
 
+    // Constructor
+    public Client(int idCliente, String name, String lastname,
+                  String mail, String password) {
+        this.idClient = idCliente;
+        this.name = name;
+        this.lastname = lastname;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    // Getters y setters
+    public int getIdCliente() { return idClient; }
+    public void setIdCliente(int idCliente) { this.idClient = idCliente; }
+
+    public String getname() { return name; }
+    public void setname(String name) { this.name = name; }
+
+    public String getlastname() { return lastname; }
+    public void setlastname(String lastname) { this.lastname = lastname; }
+
+    public String getMail() { return mail; }
+    public void setMail(String mail) { this.mail = mail; }
+
+    public String getpassword() { return password; }
+    public void setpassword(String password) { this.password = password; }
+
+    @Override
+    public String toString() {
+        return idClient + " - " + name + " " + lastname + " <" + mail + ">";
+    }
 }
